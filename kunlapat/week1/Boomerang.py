94bf283d65f659047880e1256cd348846b70bc28
jollux_ <gunkunlapat@gmail.com>
2022-08-22 10:23:09 +0700

    Add one last solution for week 1

---
+++ kunlapat/week1/Boomerang.py
@@ -0,0 +1,16 @@
+"""Prints something to console"""
+import math
+
+
+def main():
+    """The main function"""
+    numx = int(input())
+    numy = int(input())
+    numz = int(input())
+    print(numx+1)
+    print((7*numy**3) + (2*numy**2) - (31*numy) + 1)
+    print(-numz)
+    print((numx+numy)*(numx-numy))
+    print((numy-math.sqrt(numy**2-(4*numx*numz)))/(2*numx))
+
+main()
