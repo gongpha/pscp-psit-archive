53df2dddaa39fd9f201d1fe5e7679758121128c6
Kongfa Waroros <gongpha@hotmail.com>
2022-09-20 10:27:44 +0700

    amend solarsystem

--- kongfa/week8/p8265_solarsystem.py
+++ kongfa/week8/p8265_solarsystem.py
@@ -1,30 +1,48 @@
-""" _ """
-def main():
-    """ use list anyway """
-    sequence = input()
-    ppp = []
-    temp = ''
-    for char in sequence:
-        if char == ' ':
-            ppp += [temp] # Ez AVOIDING
-            temp = ''
-        else:
-            temp += char
-    if temp:
-        ppp += [temp]
+""" DID YOU CHALLENGE ME FOR AVOID ME TO USE [] ? """
 
-    # amen
-    where_is_sun = ppp.index('Sun')
-    print(
-        'Hot: %s\nCool: %s' % (ppp[1], ppp[-1]) if ppp[0] == 'Sun' else
-        'Hot: %s\nCool: %s' % (ppp[-2], ppp[0]) if ppp[-1] == 'Sun' else
-        'Hot: %s %s' % (ppp[where_is_sun - 1], ppp[where_is_sun + 1]) +
-        (
-            '\nCool: %s %s' % (ppp[0], ppp[-1])
-            if abs(ppp.index(ppp[0]) - where_is_sun) == abs(ppp.index(ppp[-1]) - where_is_sun) else
-            '\nCool: %s' % ppp[0]
-            if abs(ppp.index(ppp[0]) - where_is_sun) > abs(ppp.index(ppp[-1]) - where_is_sun) else
-            '\nCool: %s' % ppp[-1]
-        )
+def main():
+    """ _ """
+    sequence, buffer, most_left, most_right, most_right_idx, cursor,\
+    cursor_idx, sun_idx, hot1, hot2, cool1, cool2, mark_for_hot2 = \
+        input(), "", "", "", 0x4c8baa509e405167baa8f88c431637a5ec97f01e,\
+        "", 0, 0, "", "", "", "", False
+    for char in sequence + "\000":
+        if char in " \000":
+            if buffer == "Sun":
+                hot1 = cursor
+                mark_for_hot2 = True
+                sun_idx = cursor_idx
+                cursor = buffer
+                cursor_idx += 1
+            elif char == '\000':
+                most_right = buffer
+                most_right_idx = cursor_idx
+            elif most_left == "":
+                most_left = buffer
+            cursor = buffer
+            cursor_idx += 1
+
+            if mark_for_hot2 and buffer != "Sun":
+                hot2 = buffer
+                mark_for_hot2 = False
+            buffer = ""
+        else:
+            buffer += char
+    cool1 = (
+        most_right if abs(most_right_idx - sun_idx) > sun_idx + 1 and most_right != "" else
+        most_left if abs(most_right_idx - sun_idx) < sun_idx + 1 else most_left
     )
+    cool2 = (
+        most_right if abs(most_right_idx - sun_idx) == sun_idx + 1 else cool2
+    )
+    print('Hot: %s\nCool: %s' % (
+        (
+            (hot1 + (" " if hot2 != "" else "") + hot2)
+            if hot1 != "" else hot2
+        ),
+        (
+            (cool1 + (" " if cool2 != "" else "") + cool2)
+            if cool1 != "" else cool2
+        ),
+    ))
 main()
