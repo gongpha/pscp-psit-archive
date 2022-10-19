0871cddfb7fb6d86f068b8275e163ade93bd7162
jollux_ <gunkunlapat@gmail.com>
2022-10-20 06:05:29 +0700

    bunch of random stuff that I have to fix later

---
+++ kunlapat/week6/Restaurant.py
@@ -0,0 +1,13 @@
+"""I am done with my bullshit, the test is tomorrow"""
+
+
+def worth(eaten, limit, save, more) -> tuple[str, int]:
+    
+    return 
+
+if __name__ == "__main__":
+    worth_it, how_much = worth(float(input(),float(input(),float(input(),float(input())))))
+    if how_much > 0:
+        print(worth_it, how_much)
+    else:
+        print(worth_it)
